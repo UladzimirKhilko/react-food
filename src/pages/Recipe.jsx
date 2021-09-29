6fc337188201bdf395639380f2c4cfd7d1a22c8d
UladzimirKhilko <vladimirhilko@gmail.com>
2021-09-29 22:19:37 +0300

    fix video

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -60,6 +60,7 @@ function Recipe() {
                                 src={`https:\\www.youtube.com/embed/${recipe.strYoutube.slice(
                                     -11
                                 )}`}
+                                allowfullscreen
                             />
                         </div>
                     ) : null}
