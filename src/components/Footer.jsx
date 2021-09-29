786b6c90cf9e74caa654751727d47f8ad2c6b85a
UladzimirKhilko <vladimirhilko@gmail.com>
2021-09-29 22:00:12 +0300

    add link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
                     © {new Date().getFullYear()} Copyright Text
                     <a
                         className='grey-text text-lighten-4 right'
-                        href='https://github.com/UladzimirKhilko/react-shop'
+                        href='https://github.com/UladzimirKhilko/react-food'
                         target='_blank'
                         rel='noreferrer'
                     >
